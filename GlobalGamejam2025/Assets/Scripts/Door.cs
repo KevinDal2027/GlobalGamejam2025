41faf362f91a881c4eaf6a0b86d7be26e1fa669d
Adithya Olivannan <ad837157@dal.ca>
2025-01-25 14:18:26 -0400

    Added procedural rooms

---
+++ GlobalGamejam2025/Assets/Scripts/Door.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Notify the ProceduralRoomGenerator to generate new rooms
+            ProceduralRoomGenerator.Instance.OnPlayerEnterDoor();
+        }
+    }
+}
