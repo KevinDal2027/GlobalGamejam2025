832c3a52832c346c8945082854924ab5e3a7d088
Adithya Olivannan <ad837157@dal.ca>
2025-01-26 11:05:35 -0400

    Added some endgame logic

---
+++ GlobalGamejam2025/Assets/Scripts/EndRoomScript.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndRoomScript : MonoBehaviour
+{
+    SceneLoader sceneLoader;
+
+    private void Start()
+    {
+        sceneLoader = GameObject.Find("SceneLoader").GetComponent<SceneLoader>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            sceneLoader.LoadScene(2);
+        }
+    }
+}
